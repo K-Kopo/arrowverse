35c611c037fab3bc5c39ded935245aa907cb0418
Kory Koponen <koponenk@gmail.com>
2021-11-17 09:41:17 -0600

    created develop branch and added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router';
 import './App.css';
+import Characters from './Pages/Characters/Characters';
+import Main from './Pages/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/characters" element={<Characters />} />
+      </Routes>
+    
     </div>
   );
 }
---
+++ src/Pages/Characters/Characters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Characters = () => {
+    return (
+        <div>
+            <h1>WHO'S YOUR FAVORITE CHARACTER?</h1>
+        </div>
+    );
+};
+
+export default Characters;
